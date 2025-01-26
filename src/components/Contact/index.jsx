fd44163dd15a9349a6a2b81e1408623295e0e27e
johaco <johacosanchez@gmail.com>
2025-01-26 00:58:43 -0300

    implementacion de mapa con leafletjs en la page de contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,6 +3,7 @@ import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -21,20 +22,23 @@ const Contact = () => {
 
 
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs
-        .sendForm('service_9h3dabi', 'template_arn9ss1', form.current, {
-          publicKey: '6ht2Hprv76Kk1XG-N',
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_9h3dabi', 'template_arn9ss1', form.current, {
+                publicKey: '6ht2Hprv76Kk1XG-N',
+            })
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    alert('Failed to send the message, please try again')
+                    console.log('FAILED...', error.text);
+                },
+            );
     };
 
     return (
@@ -57,23 +61,41 @@ const Contact = () => {
                         <form ref={form} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
-                                    <input type="text" name="user_name" placeholder="Name" required/>
+                                    <input type="text" name="user_name" placeholder="Name" required />
                                 </li>
                                 <li className="half">
-                                    <input type="email" name="user_email" placeholder="Email" required/>
+                                    <input type="email" name="user_email" placeholder="Email" required />
                                 </li>
                                 <li>
-                                    <input type="text" name="user_subject" placeholder="Subject" required/>
+                                    <input type="text" name="user_subject" placeholder="Subject" required />
                                 </li>
                                 <li>
-                                    <textarea type="message" name="message" placeholder="Message" required/>
+                                    <textarea type="message" name="message" placeholder="Message" required />
                                 </li>
                                 <li>
-                                    <input type="submit" className="flat-button" value="Send"/>
+                                    <input type="submit" className="flat-button" value="Send" />
                                 </li>
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Joaquin Sanchez,
+                    <br />
+                    Argentina,
+                    <br />
+                    Ruta 50, Catitas Viejas <br />
+                    Santa Rosa, Mendoza <br />
+                    <br />
+                    <span>johacosanchez@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[-33.2993182, -68.0482999]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[-33.2993182, -68.0482999]}>
+                            <Popup>Joaquin lives here, come over for a mate</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type="pacman" />
