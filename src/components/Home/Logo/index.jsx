395ebb8ca3845214fd1b1939f7a5ad086cb90c71
johaco <johacosanchez@gmail.com>
2025-01-30 04:25:57 -0300

    le di estilos de responsive utilizando las dimensiones del iphone 12, falta darle estilos para que en su lugar el logo. tambien modifique los estilos que tenia al logotivo del home

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -1,70 +1,17 @@
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import LogoS from '../../../assets/images/logo-s.png';
+import 'animate.css';
+import LogoS from '../../../assets/images/logo-d-svg.svg';
 import './index.scss';
 
 const Logo = () => {
-  const bgRef = useRef();
-  const outlineLogoRef = useRef();
-  const solidLogoRef = useRef();
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      });
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    );
-  }, []);
-
   return (
-    <div className="logo-container" ref={bgRef}>
+    <div className="logo-container">
       <img
-        className="solid-logo"
-        ref={solidLogoRef}
+        className="solid-logo animate__animated animate__fadeInDown"
         src={LogoS}
-        alt="JavaScript Developer"
+        alt="Mi Logo"
       />
-
-      <svg
-        width="559pt"
-        height="897pt"
-        version="1.0"
-        viewBox="0 0 559 897"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g
-          className="svg-container"
-          transform="translate(0 457) scale(.1 -.1)"
-          fill="none"
-        >
-          <path
-            ref={outlineLogoRef}
-            d="... (el contenido del path largo)"
-          />
-        </g>
-      </svg>
     </div>
-  );
-};
+  )
+}
 
 export default Logo;
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,54 +3,66 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
 
 
 const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+
     return (
         <div className="nav-bar">
             <Link className="logo" to='/'>
                 <img src={LogoS} alt="logo" />
                 <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
             </Link>
-            <nav>
-                <NavLink exact="true" activeclassname="active" to="/">
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink
+                    onClick={() => setShowNav(false)}
+                    exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <NavLink
+                    onClick={() => setShowNav(false)}
+                    exact="true" activeclassname="active" className="about-link" to="/about">
                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className='portfolio-link' to="/portfolio">
+                <NavLink
+                    onClick={() => setShowNav(false)}
+                    exact="true" activeclassname="active" className='portfolio-link' to="/portfolio">
                     <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className='contact-link' to="/contact">
+                <NavLink
+                    onClick={() => setShowNav(false)}
+                    exact="true" activeclassname="active" className='contact-link' to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
+                <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color='#ffd700' size='3x' className='close-icon' />
             </nav>
             <ul>
                 <li>
                     <a href="https://www.linkedin.com/in/joaqu%C3%ADn-s%C3%A1nchez-12b135265/" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"  className="anchor-icon"/>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" className="anchor-icon" />
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/johaco" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"  className="anchor-icon"/>
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" className="anchor-icon" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/johaco.x?igsh=ZzlkYTZkZWVmb3J5" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"  className="anchor-icon"/>
+                        <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" className="anchor-icon" />
                     </a>
                 </li>
                 <li>
                     <a href="www.linkedin.com/in/joaquín-sánchez-12b135265" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faYoutube} color="#4d4d4e"  className="anchor-icon"/>
+                        <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" className="anchor-icon" />
                     </a>
                 </li>
             </ul>
-
+            <FontAwesomeIcon onClick={() => setShowNav(true)} icon={faBars} color='#ffd700' size='3x' className='hamburger-icon' />
         </div>
     )
 }
