e24e7bec7aa857a4f963ab34decc08eee8e9ca3e
johaco <johacosanchez@gmail.com>
2025-02-12 15:36:40 -0300

    primeros cambios del navbar para celulares, cambie la posicion de los navBar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -38,7 +38,7 @@ const Sidebar = () => {
                     exact="true" activeclassname="active" className='contact-link' to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
-                <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color='#ffd700' size='3x' className='close-icon' />
+               
             </nav>
             <ul>
                 <li>
@@ -58,6 +58,7 @@ const Sidebar = () => {
                 </li>
             </ul>
             <FontAwesomeIcon onClick={() => setShowNav(true)} icon={faBars} color='#ffd700' size='3x' className='hamburger-icon' />
+            {/* <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color='#ffd700' size='3x' className='close-icon' /> */}
         </div>
     )
 }
